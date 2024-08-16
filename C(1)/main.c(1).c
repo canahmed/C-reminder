9d5db3a0a87cfc5d8a6f315acd895ecb0c868ac5
canahmed <canahmed@icloud.com>
2024-08-16 09:30:03 +0300

    first

---
+++ C(1)/main.c(1).c
@@ -0,0 +1,16 @@
+
+/* BU BIR TEKRAR VE PRATIK BASLANGICIDIR*/
+
+#include <stdio.h>    /*Bu programda hangi kütüphanelerin, derleyicilerin çalisacagini gosterir. Kisaca on islemci drektifi denir*/
+
+int main()  /*ANA PROGRAM*/
+{
+                                                             /*ANA PROGRAM GOVDESI*/
+printf("Merhaba, Dunya");     /*YAZDIRMA FONKSIYONU*/
+
+return 0;        /*PROGRAM BASARI ILE TAMAMLANDIGINDA 0 SONUCUNU DONDUR/*                                        
+                                                             /*ANA PROGRAM GOVDESI*/
+}
+
+
+
